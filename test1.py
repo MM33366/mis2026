8fd100476ec96463f6b57424fe76957e7daa0a63
MM33366 <emma65454@gmail.com>
2026-03-10 23:34:19 +0800

    week3_py

--- test1.py
+++ test1.py
@@ -1,15 +1,12 @@
-def square(y):
-	print(f"{y}的三次方是{y**3}")
-
+def sum_n(y):
+    print(f"1 + 2 + ... + {y}的總和是{sum(range(1, y+1))}")
 
 
 x = int(input("請輸入一個整數："))
-#x += 10
 
-if (x<=0):
-	print(f"您輸入的值是{x},小於等於0")
+if x <= 0:
+    print(f"您輸入的值是 {x}, 小於等於0")
 else:
-	print(f"您輸入的值是{x},大於0")
-	for i in range(1, x+1):
-		#print (i, end=";")
-		square(i)
+    print(f"您輸入的值是 {x}, 大於0")
+    for i in range(1, x+1):
+        sum_n(i)
